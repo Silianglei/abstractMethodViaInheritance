aaa22a30100e3bf3653536d0252db7ed897e1b56
SiLiang Lei <slei01@stuy.edu>
2019-03-03 04:41:15 -0500

    1sr edit

---
+++ UserOfSpeakers.java
@@ -0,0 +1,9 @@
+public class UserOfSpeakers {
+  public static void main(String[] commandLine){
+    Retriever r = new Retriever();
+    Animal a = r;
+    System.out.println(a.speak());
+    
+  }
+
+}
